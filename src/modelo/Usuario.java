7d8b548203c1534fd011348388cfbd6dbefc07e5
LAB II - PC 01 <lautarorabinovich.et36@gmail.com>
2026-06-19 09:35:39 -0300

    ComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollasComoCocinarCentollas

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -6,13 +6,12 @@ public class Usuario {
     private String nombre;
     private String apellido;
     private String mail;
-    private ArrayList<Prestamo> prestamos;
+    private ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>();
 
     public Usuario(String nombre, String apellido, String mail) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.mail = mail;
-        prestamos = new ArrayList<Prestamo>();
     }
 
     public void agregarPrestamo(Prestamo prestamo){
--- src/vista/panels/MainMenu.java
+++ src/vista/panels/MainMenu.java
@@ -4,44 +4,42 @@ import vista.Vista;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainMenu { //
-    private JPanel menu;
-
+public class MainMenu extends JPanel{ //
     private JButton btnLibro, btnRevista, btnDVD, btnEscape;
 
     public MainMenu() {
-        menu = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.insets = new Insets(5, 5, 5, 5);
+        setLayout(new GridBagLayout()); //Cambio el layout
+        GridBagConstraints c = new GridBagConstraints(); //Creo el objeto que tiene los atributos de posicion de los demas componentes
+        c.insets = new Insets(5, 5, 5, 5); //Padding default en todos los elementos
 
 
 
 //      ---Estructura
         btnEscape = new JButton("Escape");
-        c.weightx = .5;
-        c.ipady = 20;
+        c.weightx = .5; //Le indica al componente que ocupe t0do el ancho de su contenedor
+        c.ipady = 20; //Padding interno del componente
         c.anchor = GridBagConstraints.FIRST_LINE_START;
         c.gridx = 0; c.gridy = 0;
-        menu.add(btnEscape, c);
+        add(btnEscape, c);
 
 
 
         btnLibro = new JButton("Libro");
         c.weighty = .5;
-        c.weightx = .5;
+        c.weightx = 1;
         c.ipady = 0;
         c.fill = GridBagConstraints.BOTH;
         c.anchor = GridBagConstraints.CENTER;
         c.gridx = 0; c.gridy = 1;
-        menu.add(btnLibro, c);
+        add(btnLibro, c);
 
         btnRevista = new JButton("Revista");
         c.gridx = 1; c.gridy = 1;
-        menu.add(btnRevista, c);
+        add(btnRevista, c);
 
         btnDVD = new JButton("DVD");
         c.gridx = 2; c.gridy = 1;
-        menu.add(btnDVD, c);
+        add(btnDVD, c);
 
 
 
@@ -55,7 +53,7 @@ public class MainMenu { //
         c.anchor = GridBagConstraints.CENTER;
         c.gridy = 2; c.gridx = 0; c.gridwidth = 3;
         ImageIcon imagen = new ImageIcon("./assets/test.png");
-        menu.add(lblTitulo, c);
+        add(lblTitulo, c);
 
 //      ---Design
 
@@ -82,7 +80,4 @@ public MainMenu(){
     c.insets = new Insets(5, 5, 5, 5);
 
 }*/
-    public JPanel getMenu() {
-        return menu;
-    }
 }
