8f25fdd3f19a2624423634d207b676d172be76d1
Cross <santitopalacios778@gmail.com>
2026-07-01 08:24:23 -0300

    upd controlador

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -5,8 +5,8 @@ import modelo.Usuario;
 import vista.Vista;
 
 public class Controlador {
-    private Vista vista = new Vista(500, 500);
-    private Modelo modelo = new Modelo();
+    private Vista vista;
+    private Modelo modelo;
 
     // Constructor
     public Controlador(Vista vista, Modelo modelo) {
